19bde10e42ca8c99f33fc47a288c6bf5b8525fd2
akki697222 <newakki69@outlook.jp>
2024-02-25 00:16:44 +0900

    ファイル構造を変更

---
+++ ccnet/test/testgui_1.lua
@@ -0,0 +1,17 @@
+local basalt = require("basalt")
+
+local main = basalt.createFrame()
+local button = main --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
+        :addButton() --> This is an example of call chaining
+        :setPosition(4, 4)
+        :setText("Click me!")
+        :onClick(
+            function()
+                basalt.debug("I got clicked!")
+            end)
+local label = main
+        :addLabel()
+        :setText("akki_kayo")
+        :setFontSize(3)
+
+basalt.autoUpdate()
