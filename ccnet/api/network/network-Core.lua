80315b89c54b4054fb66a9a74da6e633973ef6f0
akki697222 <newakki69@outlook.jp>
2024-02-25 17:14:11 +0900

    modemがちゃんとopenするよう修正

--- ccnet/api/network/network-Core.lua
+++ ccnet/api/network/network-Core.lua
@@ -3,15 +3,27 @@ local config = require("/api/config/network-Core-Config")
 local network = {}
 
 function network.transmit(channel, packet)
+    modem.open(config.getUserChannel())
     modem.transmit(channel, config.getUserChannel(), packet)
+    modem.close(config.getUserChannel())
 end
 
 function network.receive()
+    modem.open(config.getUserChannel())
     local event, side, channel, replyChannel, message, distance
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == config.getUserChannel()
+    modem.close(config.getUserChannel())
     return message
+end
+
+function network.open(channel)
+    modem.open(channel)
+end
+
+function network.close(channel)
+    modem.close(channel)
 end
 
 return network
