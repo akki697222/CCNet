9b3ac6bc3eb6f7592c99e530c784b4c7677f89d1
akki697222 <newakki69@outlook.jp>
2024-02-25 12:43:38 +0900

    修正

--- ccnet/api/config/network-Core-Config.lua
+++ ccnet/api/config/network-Core-Config.lua
@@ -1,5 +1,8 @@
 local config = {}
 
 local USER_CHANNEL = 00000
+function config.setChannel(number)
+    USER_CHANNEL = number
+end
 
 return config
--- ccnet/gui/guis/configurator.lua
+++ ccnet/gui/guis/configurator.lua
@@ -1,17 +1,21 @@
 local basalt = require("/gui/basalt")
 local config = require("/api/config/network-Core-Config")
+local log = require("/api/logger-Core")
 
 local main = basalt.createFrame():setTheme({FrameBG = colors.white})
 --main window
 main:addLabel():setText("CCNet - Configurator"):setPosition(2, 2):setFontSize(1)
 main:addPane():setSize(51, 1):setPosition(1, 2):setBackground(colors.blue)
 main:addButton():setSize(3, 1):setPosition(48, 2):setText(" x "):setBackground(colors.red):onClick(function()shell.run("/gui/guis/controlpanel")end)
---main
-main:addLabel():setText("Config - User Channel: "):setPosition(7, 4)
-main:addInput():setInputType("number"):setDefaultText("00000"):setInputLimit(5):setPosition(30, 4)
-main:addButton():setSize(5, 1):setPosition(2, 4):setText(" SET "):setBackground(colors.red):onClick(function()
-    config.USER_CHANNEL = main:getInputType()
-    shell.run("/gui/guis/configurator")
+--input
+main:addLabel():setText("Config - Network"):setPosition(2, 4)
+local sub = main:addFrame():setPosition(2,5):setSize(16, 50)
+sub:addProgram():setPosition(2,5):setSize(16, 50):execute(function ()
+    shell.run("edit /api/config/network-Core-Config.lua")
+end)
+--setconfig
+main:addButton():setSize(5,1):setPosition(45, 18):setText(" SET "):setBackground(colors.red):onClick(function ()
+    
 end)
 
 basalt.autoUpdate()
