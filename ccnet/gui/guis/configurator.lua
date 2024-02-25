3a95e542db3ca65f60fad28fddd9a4517013bd2a
akki697222 <newakki69@outlook.jp>
2024-02-25 13:13:44 +0900

    configuratorの大規模な修正&UIを一部変更

--- ccnet/gui/guis/configurator.lua
+++ ccnet/gui/guis/configurator.lua
@@ -1,21 +1,44 @@
 local basalt = require("/gui/basalt")
 local config = require("/api/config/network-Core-Config")
 local log = require("/api/logger-Core")
+local main = basalt.createFrame()
 
-local main = basalt.createFrame():setTheme({FrameBG = colors.white})
+local sub = {
+    main:addFrame():setPosition(1,3):setSize(51, 17),
+    main:addFrame():setPosition(1,3):setSize(51, 17):hide()
+}
+
+local function openSubFrame(id) -- we create a function which switches the frame for us
+    if(sub[id]~=nil)then
+        for k,v in pairs(sub)do
+            v:hide()
+        end
+        sub[id]:show()
+    end
+end
+
+local menubar = main:addMenubar():setScrollable() -- we create a menubar in our main frame.
+    :setSize(51, 1)
+    :setPosition(1, 2)
+    :onChange(function(self, val)
+        openSubFrame(self:getItemIndex()) -- here we open the sub frame based on the table index
+    end)
+    :addItem("Network")
+    :addItem("Logger")
+
 --main window
-main:addLabel():setText("CCNet - Configurator"):setPosition(2, 2):setFontSize(1)
-main:addPane():setSize(51, 1):setPosition(1, 2):setBackground(colors.blue)
-main:addButton():setSize(3, 1):setPosition(48, 2):setText(" x "):setBackground(colors.red):onClick(function()shell.run("/gui/guis/controlpanel")end)
+main:addLabel():setText("CCNet - Configurator"):setPosition(2, 1):setFontSize(1)
+main:addPane():setSize(51, 1):setPosition(1, 1):setBackground(colors.blue)
+main:addButton():setSize(3, 1):setPosition(48, 1):setText(" x "):setBackground(colors.red):onClick(function()shell.run("/gui/guis/controlpanel")end)
 --input
-main:addLabel():setText("Config - Network"):setPosition(2, 4)
-local sub = main:addFrame():setPosition(2,5):setSize(16, 50)
-sub:addProgram():setPosition(2,5):setSize(16, 50):execute(function ()
+sub[1]:addLabel():setText("Config - Network"):setPosition(2, 17)
+sub[1]:addProgram():setPosition(1,1):setSize(51, 16):execute(function ()
     shell.run("edit /api/config/network-Core-Config.lua")
 end)
---setconfig
-main:addButton():setSize(5,1):setPosition(45, 18):setText(" SET "):setBackground(colors.red):onClick(function ()
-    
+
+sub[2]:addLabel():setText("Config - Logger"):setPosition(2, 17)
+sub[2]:addProgram():setPosition(1,1):setSize(51, 16):execute(function ()
+    shell.run("edit /api/config/logger-Core-Config.lua")
 end)
 
 basalt.autoUpdate()
--- ccnet/gui/guis/controlpanel.lua
+++ ccnet/gui/guis/controlpanel.lua
@@ -1,9 +1,9 @@
 local basalt = require("/gui/basalt")
 
-local main = basalt.createFrame():setTheme({FrameBG = colors.white})
+local main = basalt.createFrame()
 --main window
-main:addLabel():setText("CCNet - Control Panel"):setPosition(2, 2):setFontSize(1)
-main:addPane():setSize(51, 1):setPosition(1, 2):setBackground(colors.blue)
+main:addLabel():setText("CCNet - Control Panel"):setPosition(2, 1):setFontSize(1)
+main:addPane():setSize(51, 1):setPosition(1, 1):setBackground(colors.blue)
 --operation buttons
 main:addButton():setSize(19, 1):setPosition(2, 16):setText("Open Configurator"):onClick(function()shell.run("/gui/guis/configurator")end)
 main:addButton():setSize(17, 1):setPosition(2, 18):setText("Shutdown System"):onClick(
