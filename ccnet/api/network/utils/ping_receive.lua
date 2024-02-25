53e23d3c8de4a9053570bf59ab161d935bc7c739
akki697222 <newakki69@outlook.jp>
2024-02-25 17:16:54 +0900

    修正

--- ccnet/api/network/utils/ping_receive.lua
+++ ccnet/api/network/utils/ping_receive.lua
@@ -1,5 +1,4 @@
 local network = require("/api/network/network-Core")
-local config = require("/api/config/network-Core-Config")
 if network.receive() == "ping" then
     print("Ping received. returning pings.")
     network.transmit(32001, "ping")
