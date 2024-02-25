3c9e4b2a45bef8a939ecbd476df168c2bd6aab7f
akki697222 <newakki69@outlook.jp>
2024-02-25 16:59:59 +0900

    修正

--- ccnet/api/network/network-Core.lua
+++ ccnet/api/network/network-Core.lua
@@ -1,17 +1,16 @@
 local modem = peripheral.find("modem") or error("modem not found", 0)
 local config = require("/api/config/network-Core-Config")
-local channels = config.getUserChannel
 local network = {}
 
 function network.transmit(channel, packet)
-    modem.transmit(channel, channels, packet)
+    modem.transmit(channel, config.getUserChannel(), packet)
 end
 
 function network.receive()
     local event, side, channel, replyChannel, message, distance
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    until channel == channels
+    until channel == config.getUserChannel()
     return message
 end
 
