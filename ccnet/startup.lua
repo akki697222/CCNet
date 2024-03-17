e80c94dfe48fa472fe05a1b23fb49b895199cde1
2 1 <InotitoKokoro@gmail.com>
2024-03-17 17:24:12 +0900

    log作成中
    一回put

--- ccnet/startup.lua
+++ ccnet/startup.lua
@@ -2,4 +2,9 @@ local api = require("/api/api-Core")
 
 api.load()
 
+if not fs.exists("log.txt") then
+    local file = fs.open("log.txt", "w")
+    file.write(os.time("local"))
+end
+
 shell.run("/gui/guis/controlpanel")
